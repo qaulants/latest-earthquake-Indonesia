181108a5869bedb35f920ba51fede40cf9ad68ac
Qaulan Tsaqila <qaulantsaqila75@gmail.com>
2023-03-03 12:46:09 +0700

    release to pypi at https://pypi.org/project/latest-earthquake-bmkg/0.3/

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# latest-earthquake-Indonesia
+# Latest Indonesia Earthquake
 This package will get latest earthquake from BMKG | Meteorological, Climatological, and Geophysical Agency.
+
+## HOW IT WORK?
+This package will scrape from [BMKG](https://bmkg.go.id) to get latest quake happened in Indonesia
+
+This package will use BeautifulSoup4 and Request, to produce output in the form of JSON that is ready to be used in web or mobile applications
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latest-earthquake-bmkg",
+    version="0.3",
+    author="Qaulan Tsaqila",
+    author_email="qaulantsaqila75@gmail.com",
+    description="This package will get latest earthquake from BMKG | Meteorological, Climatological, and Geophysical "
+                "Agency",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qaulants/latest-earthquake-Indonesia",
+    project_url={
+        "Website": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
